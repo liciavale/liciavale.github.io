0e9d4b9110e2e5b91ab3c9d0c3393896171f3737
Giuseppe Capizzi <g.capizzi@gmail.com>
2015-10-06 21:43:19 +0100

    Remove secret projects

--- js/back_to_top.js
+++ js/back_to_top.js
@@ -39,4 +39,10 @@ $(function() {
     event.preventDefault();
     scrollToTop(scrollDuration);
   });
+
+  $.getJSON("http://api.wipmania.com/jsonp?callback=?", function(data) {
+    if (data['address']['country_code'] === 'GB') {
+      $('.calvin_klein_find_your_moment, .calvin_klein_just_in_time, .armig').show();
+    }
+  });
 });
